b45d2546eaba456cb027ad17c7cf09ed625b6fb1
ParkSeyang <ghlwkd14@naver.com>
2025-02-14 19:49:23 +0900

    (2025.02.14) 양방향 연결 리스트

--- Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
+++ Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
@@ -56,13 +56,82 @@ public:
 		}
 		else
 		{
+			Node* currentNode = head;
+
+			while (currentNode->next != nullptr)
+			{
+				currentNode = currentNode->next;
+			}
+			currentNode->next = newNode;
+
 			newNode->data = data;
-			newNode->next = head;
-			head = newNode;
-			currentNode;
+			newNode->next = nullptr;
 		}
 	}
 	
+	void pop_front()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		else
+		{
+			Node* deleteNode = head;
+
+			head = deleteNode->next;
+
+			delete deleteNode;
+			
+			size--;
+		}
+	}
+	void Show()
+	{
+		Node* currentNode = head;
+		while (currentNode != nullptr)
+		{
+			cout << "Node Data : " << currentNode->data << endl;
+
+			currentNode = currentNode->next;
+		}
+	}
+
+	void pop_back()
+	{
+		
+
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		if (head->next == nullptr)
+		{
+			delete head;
+			head = nullptr;
+		}
+		else
+		{
+			Node* deleteNode = new Node;
+
+			Node* previousNode = new Node;
+
+			previousNode = head;
+
+			while (previousNode->next != nullptr && previousNode->next->next != nullptr)
+			{
+				previousNode = previousNode->next;
+
+			}
+			deleteNode = previousNode->next;
+			
+			previousNode->next = nullptr;
+
+		}
+
+		size--;
+	}
+
 };
 
 
@@ -70,10 +139,19 @@ public:
 int main()
 {
 	SingleLinkedList<int> singleLinkedList;
-	
+
 	singleLinkedList.push_front(10);
 	singleLinkedList.push_front(20);
+	singleLinkedList.push_back(5);
+	singleLinkedList.push_back(10);
+	
+	singleLinkedList.Show();
 
+	singleLinkedList.pop_back();
+	singleLinkedList.pop_back();
+	singleLinkedList.pop_back();
+	singleLinkedList.pop_back();
+	singleLinkedList.pop_back();
     return 0;
 }
 
