b2efce7099b5c62fa95fd50e11d8864c7483e4f5
ParkSeyang <ghlwkd14@naver.com>
2025-02-17 19:44:36 +0900

    (2025.02.17) 원형 연결 리스트

--- Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
+++ Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
@@ -2,159 +2,54 @@
 
 using namespace std;
 
-template <typename T>
-
-class SingleLinkedList
+template<typename T>
+class DoubleLinkedList
 {
 private:
 	int size;
-
+	
 	struct Node
 	{
-		T data;
-		Node * next;
+		T data;	
+		Node* next;
+		Node* previous;
 	};
-
-	Node * head;
-
+	
+	Node* head;
+	Node* tail;
 
 public:
-	SingleLinkedList()
+	DoubleLinkedList()
 	{
 		size = 0;
 		head = nullptr;
+		tail = nullptr;
 	}
 
 	void push_front(T data)
 	{
 		Node* newNode = new Node;
+
 		if (head == nullptr)
 		{
-			head = newNode;
-
-			newNode->data = data;
-			newNode->next = nullptr;
-
+			head = newNode->data;
+			tail = newNode->data;
 		}
 		else
 		{
-			newNode->data = data;
-			newNode->next = head;
-			head = newNode;
+			newNode->previous = newNode->next;
+			newNode->next = previous;
+			newNode->next = nullptr;
 		}
-
 		size++;
 	}
-	void push_back(T data)
-	{
-		Node* newNode = new Node;
-		if (head == nullptr)
-		{
-			head = newNode;
-			newNode->data = data;
-			newNode->next = nullptr;
-		}
-		else
-		{
-			Node* currentNode = head;
 
-			while (currentNode->next != nullptr)
-			{
-				currentNode = currentNode->next;
-			}
-			currentNode->next = newNode;
-
-			newNode->data = data;
-			newNode->next = nullptr;
-		}
-	}
-	
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "Linked List is Empty" << endl;
-		}
-		else
-		{
-			Node* deleteNode = head;
-
-			head = deleteNode->next;
-
-			delete deleteNode;
-			
-			size--;
-		}
-	}
-	void Show()
-	{
-		Node* currentNode = head;
-		while (currentNode != nullptr)
-		{
-			cout << "Node Data : " << currentNode->data << endl;
-
-			currentNode = currentNode->next;
-		}
-	}
-
-	void pop_back()
-	{
-		if (head == nullptr)
-		{
-			cout << endl << "Linked List is Empty (pop_back)" << endl;
-			return;
-		}
-		else if (head->next == nullptr)
-		{
-			Node* deleteNode = head;
-			head = nullptr;
-			delete deleteNode;
-		}
-		else
-		{
-			Node* previousNode = head;
-			Node* deleteNode = previousNode->next;
-
-			while (deleteNode->next != nullptr)
-			{
-				deleteNode = deleteNode->next;
-				previousNode = previousNode->next;
-			}
-			previousNode->next = nullptr;
-			delete deleteNode;
-		}
-
-		size--;
-
-	}
-	
 };
-
 
 
 int main()
 {
-	SingleLinkedList<int> singleLinkedList;
-
-	singleLinkedList.push_front(10);
-	singleLinkedList.push_front(20);
-
-	singleLinkedList.push_back(5);
-	singleLinkedList.push_back(10);
-	
-	singleLinkedList.Show();
-
-	singleLinkedList.pop_back();
-
-	cout << endl;
-
-	singleLinkedList.Show();
-
-	singleLinkedList.pop_back();
-	singleLinkedList.pop_back();
-	singleLinkedList.pop_back();
-	singleLinkedList.pop_back();
-
-    return 0;
+	DoubleLinkedList<int> doubleLinkedList;
+	doubleLinkedList.push_front(5);
+	return 0;
 }
-
