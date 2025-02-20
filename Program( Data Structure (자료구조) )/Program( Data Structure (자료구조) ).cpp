2707968f2fd340833b8510b936218eac3bb884c2
ParkSeyang <ghlwkd14@naver.com>
2025-02-20 19:44:52 +0900

    (2025.02.20) 원형 큐

--- Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
+++ Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
@@ -44,17 +44,118 @@ public:
 		size++;
 	}
 
-	void show()
+	void push_front(T data)
 	{
-		Node* currentNode = head;
-		do
-		{
-			cout << "CurrentNode Data : " << currentNode->data << endl;
+		Node* newNode = new Node;
 
-			currentNode = currentNode->next;
-		} while (currentNode != head);
+		newNode->data = data;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->next = head;
+		}
+		else
+		{
+			newNode->next = head->next;
 	
+			head->next = newNode;
+			
+		}
+		size++;
 	}
+	
+	void pop_back()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty(Pop_back) " << endl;
+		}
+		else
+		{
+			Node* deleteNode = head;
+			Node* currentNode = head;
+			if (true)
+			{
+				
+			}
+			else
+			{
+				currentNode->next->next =
+			}
+			size--;
+
+			delete deleteNode;
+		}
+	}
+
+	void pop_front()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty(Pop_front) " << endl;
+		}
+		else
+		{
+			Node* deleteNode = head->next; // delete노드가 헤드를 가리킴
+			if (head == head->next)
+			{
+				head = nullptr;
+			}
+			// if (size == 1) // 노드가 한개일때 head를 nullptr로 해줌
+			// {
+			// 	head = nullptr;
+			// }
+			else // 노드가 두개이상일때 
+			{
+				head->next = deleteNode->next;
+			}
+			delete deleteNode;
+
+			size--;
+		}
+
+	}
+	~CircleLinkedList()
+	{
+		while (head != nullptr)
+		{
+			pop_front();
+		}
+	}
+	const int& Size()
+	{
+		return size;
+	}
+
+	 void show()
+	 {
+	 	if (head != nullptr)
+	 	{
+	 		Node* currentNode = head->next;
+	 
+	 		for (int i = 0; i < size; i++)
+	 		{
+	 			cout << currentNode->data << " ";
+	 
+	 			currentNode = currentNode->next;
+	 		}
+	 
+	 	}
+	 }
+	 
+	// void show()
+	// {
+	// 	 Node* currentNode = head->next;
+	// 	do
+	// 	{
+	// 		cout << "CurrentNode Data : " << currentNode->data << endl;
+	// 
+	// 		currentNode = currentNode->next;
+	// 
+	// 	} while (currentNode != head->next);
+	// 
+	// }
 };
 
 
@@ -67,6 +168,26 @@ int main()
 	circleLinkedList.push_back(20);
 	circleLinkedList.push_back(30);
 
+	cout << endl;
+
+	circleLinkedList.pop_front();
+	circleLinkedList.pop_front();
+	circleLinkedList.pop_front();
+	circleLinkedList.pop_front();
+
+	cout << endl;
+
 	circleLinkedList.show();
+
+
+	circleLinkedList.push_front(5);
+	
+	circleLinkedList.push_front(10);
+	
+	circleLinkedList.push_front(15);
+	
+	circleLinkedList.show();
+
+
 	return 0;
 }
