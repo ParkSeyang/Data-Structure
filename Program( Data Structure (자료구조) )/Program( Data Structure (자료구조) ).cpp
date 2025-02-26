bccba9faa18e391b7c8ca9b2acdc555d5334b8d9
ParkSeyang <ghlwkd14@naver.com>
2025-02-26 17:54:54 +0900

    복습

--- Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
+++ Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
@@ -20,30 +20,44 @@ public:
 	}
 	void push_back(T data)
 	{
-		if ()
+		if (size == capacity)
 		{
-
+			resize(capacity * 2);
 		}
-
+		container[size] = data;
+		size++;
 	}
 	void resize(int NewSize)
 	{
-		T* container = new container;
-		if (container != nullptr)
-		{
-	
-		}
-		else
-		{
+		T* newContainer = new T[NewSize];
 
+		if (size <= capacity)
+		{
+			container = NULL;
 		}
+		for (int i = 0; i < size; i++)
+		{
+			newContainer[i] = container[i];
+		}
+
+		delete container;
+		container = newContainer;
+
+		size = NewSize;
 	}
-
+	
 };
 
 
 int main()
 {
-	
+	Vector<int> vector;
+	vector.resize(4);
+	vector.push_back(1);
+	vector.push_back(2);
+	vector.push_back(3);
+	vector.push_back(4);
+
+
 	return 0;
 }
