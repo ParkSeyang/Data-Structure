f60c700d8c23f2cfc222b61ca456ac9b53709164
ParkSeyang <ghlwkd14@naver.com>
2025-02-26 19:44:20 +0900

    (2025.02.26) 우선 순위 큐

--- Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
+++ Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
@@ -20,44 +20,125 @@ public:
 	}
 	void push_back(T data)
 	{
-		if (size == capacity)
-		{
-			resize(capacity * 2);
-		}
-		container[size] = data;
-		size++;
-	}
-	void resize(int NewSize)
-	{
-		T* newContainer = new T[NewSize];
+		 if (capacity <= 0)
+		 {
+		 	resize(1);
+		 }
+		 else if (size >= capacity)
+		 {
+		 	resize(capacity * 2);
+		 }
+		 
+		 container[size++] = data;
 
-		if (size <= capacity)
+		//  if (size == capacity) 
+		//  {
+		//  	resize(capacity * 2);
+		//  }
+		//  else if ()
+		//  {
+		// 
+		//  }
+		//  container[size] = data;
+		//  size++;
+	}
+	void resize(int newSize)
+	{
+		// 1. capacity에 새로운 size 값을 저장합니다.
+
+		capacity = newSize;
+
+		// 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
+		//	  메모리 공간을 가리키도록 합니다.
+
+		T* newContainer = new T[capacity];
+
+		// 3. 새로운 메모리 공간의 값을 초기화 합니다.
+		for (int i = 0; i < capacity; i++)
 		{
-			container = NULL;
+			newContainer[i] = NULL;
 		}
+
+		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열에
+		//	  넣어 줍니다.
 		for (int i = 0; i < size; i++)
 		{
 			newContainer[i] = container[i];
+
 		}
 
-		delete container;
-		container = newContainer;
+		// 5. 기존 배열의 메모리를 해제합니다.
+		if (container != nullptr)
+		{
+			delete [ ] container;
+		}
 
-		size = NewSize;
+		// 6. 기존에 배열을 가리키던 포인터 변수의 값을
+		//    새로운 배열의 시작주소로 가리킵니다.
+		container = newContainer;
+		
 	}
-	
+	void pop_back()
+	{
+		if (size <= 0)
+		{
+			cout << "Vector is empty " << endl;
+		}
+		else
+		{
+			container[--size] = NULL;
+		}
+		// else if (container != nullptr) // 예제 2 
+		// {
+		// 	container[--size] = NULL;
+		// }
+		
+	}
+	~Vector()
+	{	
+	  if(container != nullptr)
+	  {
+		delete [ ] container;
+	  }
+	}
+
+	const int& Size()
+	{
+		return size;
+	}
+	const T & operator [] (const int& index)
+	{
+		return container[index];
+	}
+
+
 };
 
 
 int main()
 {
 	Vector<int> vector;
-	vector.resize(4);
-	vector.push_back(1);
-	vector.push_back(2);
-	vector.push_back(3);
-	vector.push_back(4);
 
+	vector.push_back(10);
+	vector.push_back(20);
+	vector.push_back(30);
+	vector.push_back(40);
+	vector.push_back(50);
+	vector.push_back(60);
+	vector.push_back(70);
+	vector.push_back(80);
+	vector.push_back(90);
+	vector.push_back(100);
+	
+	
+	
+
+
+
+	for (int i = 0; i<vector.Size(); i++)
+	{
+		cout << vector[i] << " ";
+	}
 
 	return 0;
 }
