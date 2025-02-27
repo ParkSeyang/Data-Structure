42f4c10d959ea7444e4285b0cd7028ae72fcfd5d
ParkSeyang <ghlwkd14@naver.com>
2025-02-27 19:45:18 +0900

    (2025.02.27) 이진 트리

--- Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
+++ Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
@@ -2,143 +2,87 @@
 
 using namespace std;
 
-template<typename T>
-
-class Vector
+class String
 {
 private:
+	char* pointer;
 	int size;
-	int capacity;
-	T* container;
+
 public:
-	Vector()
+	String()
 	{
 		size = 0;
-		capacity = 0;
-		container = nullptr;
-
+		pointer = nullptr;
 	}
-	void push_back(T data)
+	void operator=(const char * word)
 	{
-		 if (capacity <= 0)
-		 {
-		 	resize(1);
-		 }
-		 else if (size >= capacity)
-		 {
-		 	resize(capacity * 2);
-		 }
-		 
-		 container[size++] = data;
-
-		//  if (size == capacity) 
-		//  {
-		//  	resize(capacity * 2);
-		//  }
-		//  else if ()
-		//  {
-		// 
-		//  }
-		//  container[size] = data;
-		//  size++;
-	}
-	void resize(int newSize)
-	{
-		// 1. capacity에 새로운 size 값을 저장합니다.
-
-		capacity = newSize;
-
-		// 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
-		//	  메모리 공간을 가리키도록 합니다.
-
-		T* newContainer = new T[capacity];
-
-		// 3. 새로운 메모리 공간의 값을 초기화 합니다.
-		for (int i = 0; i < capacity; i++)
+		size = strlen(word) + 1;
+		if (pointer == nullptr)
 		{
-			newContainer[i] = NULL;
-		}
+			pointer = new char[size];
 
-		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열에
-		//	  넣어 줍니다.
-		for (int i = 0; i < size; i++)
-		{
-			newContainer[i] = container[i];
-
-		}
-
-		// 5. 기존 배열의 메모리를 해제합니다.
-		if (container != nullptr)
-		{
-			delete [ ] container;
-		}
-
-		// 6. 기존에 배열을 가리키던 포인터 변수의 값을
-		//    새로운 배열의 시작주소로 가리킵니다.
-		container = newContainer;
-		
-	}
-	void pop_back()
-	{
-		if (size <= 0)
-		{
-			cout << "Vector is empty " << endl;
+			for (int i = 0; i < size; i++)
+			{
+				pointer[i] = word[i];
+			}
 		}
 		else
 		{
-			container[--size] = NULL;
-		}
-		// else if (container != nullptr) // 예제 2 
-		// {
-		// 	container[--size] = NULL;
-		// }
-		
-	}
-	~Vector()
-	{	
-	  if(container != nullptr)
-	  {
-		delete [ ] container;
-	  }
-	}
+			char * newPointer = new char[size];
 
+			for (int i = 0; i < size; i++)
+			{
+				newPointer[i] = word[i];
+			}
+
+			delete [ ] pointer;
+
+			pointer = newPointer;
+		
+		}
+	}
+	const char& operator [ ] (const char & index)
+	{
+		return pointer[index];
+	}
 	const int& Size()
 	{
-		return size;
+		return size - 1;
 	}
-	const T & operator [] (const int& index)
+	~String()
 	{
-		return container[index];
+		if (pointer != nullptr)
+		{
+			delete [ ] pointer;
+		}
+		
 	}
-
 
 };
 
-
 int main()
 {
-	Vector<int> vector;
+	String string;
 
-	vector.push_back(10);
-	vector.push_back(20);
-	vector.push_back(30);
-	vector.push_back(40);
-	vector.push_back(50);
-	vector.push_back(60);
-	vector.push_back(70);
-	vector.push_back(80);
-	vector.push_back(90);
-	vector.push_back(100);
-	
-	
-	
+	string = "Key";
 
-
-
-	for (int i = 0; i<vector.Size(); i++)
+	for (int i = 0; i<string.Size(); i++)
 	{
-		cout << vector[i] << " ";
+		cout << string[i];
 	}
+
+	cout << endl;
+
+	string = "Apple";
+
+	for (int i = 0; i < string.Size(); i++)
+	{
+		cout << string[i];
+	}
+
+	cout << endl;
+
+	cout << "String Size : " << string.Size() << endl;
 
 	return 0;
 }
