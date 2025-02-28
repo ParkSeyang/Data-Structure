ac4a2a1bfe158c63ecad5685a98fb6414a44231a
ParkSeyang <ghlwkd14@naver.com>
2025-02-28 19:44:37 +0900

    (2025.02.28) 편향 이진 트리

--- Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
+++ Program( Data Structure (자료구조) )/Program( Data Structure (자료구조) ).cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#include <cstring>
 using namespace std;
 
 class String
@@ -41,14 +41,72 @@ public:
 		
 		}
 	}
+
 	const char& operator [ ] (const char & index)
 	{
 		return pointer[index];
 	}
+
+	void Append(const char * word)
+	{
+		int resize = size;
+
+		// strlen(pointer) + strlen(word) +1;
+		
+		size = size + strlen(word) + 1;
+
+		char* newPointer = new char[size];
+		
+		// strcpy(newPointer, pointer);
+
+		// strcpy(newPointer, word);
+
+		 for (int i = 0; i < resize; i++)
+		 {
+		 	newPointer[i] = pointer[i];
+		 }
+		 
+		 for (int i = 0; i < strlen(word) + 1; i++)
+		 {
+		 	newPointer[resize + i] = word[i];
+		 }
+
+		if (pointer != nullptr)
+		{
+			delete[] pointer;
+		}
+		pointer = newPointer;
+
+		// size = newSize;
+	}
+
+	long long Find(const char* word)
+	{
+		
+		for (int i = 0; i < strlen(word); i++)
+		{
+			if (pointer != word)
+			{
+				for (int i = 0; i < size; i++)
+				{
+
+				}
+			}
+			else if ()
+			{
+				for ()
+				{
+
+				}
+			}
+		}
+	}
+
 	const int& Size()
 	{
 		return size - 1;
 	}
+
 	~String()
 	{
 		if (pointer != nullptr)
@@ -62,27 +120,48 @@ public:
 
 int main()
 {
-	String string;
+	 String string;
+	 
+	 string = "Key";
+	 
+	 for (int i = 0; i<string.Size(); i++)
+	 {
+	 	cout << string[i];
+	 }
+	 
+	 cout << endl;
+	 
+	 string = "Apple";
+	 
+	 for (int i = 0; i < string.Size(); i++)
+	 {
+	 	cout << string[i];
+	 }
+	 
+	 cout << endl;
+	 
+	 string.Append(" Four");
+	 string.Append(" x");
 
-	string = "Key";
 
-	for (int i = 0; i<string.Size(); i++)
-	{
-		cout << string[i];
-	}
+	 for (int i = 0; i < string.Size(); i++)
+	 {
+		 cout << string[i];
+	 }
 
-	cout << endl;
+	 cout << "String Size : " << string.Size() << endl;
 
-	string = "Apple";
+	 cout << endl;
 
-	for (int i = 0; i < string.Size(); i++)
-	{
-		cout << string[i];
-	}
+	 
 
-	cout << endl;
-
-	cout << "String Size : " << string.Size() << endl;
-
+	// std::string str;
+	// str.append("HEllo");
+	// 
+	// for (int i = 0; i < str.size(); i++)
+	// {
+	// 	cout << str[i] << endl;
+	// }
+	
 	return 0;
 }
